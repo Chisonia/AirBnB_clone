4d657a05a843a14b70a028054b422e65c24528bc
root <sonya12slyvia@gmail.com>
2024-05-19 04:29:47 -0700

    still editing to pass pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''This module is the Amenity class'''
-from models.base_model import BaseModel 
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
--- models/base_model.py
+++ models/base_model.py
@@ -19,36 +19,30 @@ class BaseModel:
                 if key != '__class__':
                     try:
                         setattr(self, key, value)
-                    except:
-                        raise ValueError
+                    except raise ValueError
 
-           
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-        
-
 
     def __str__(self) -> str:
-       '''Function to return attribute as string'''
+        '''Function to return attribute as string'''
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+                )
 
-       return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
     def save(self) -> None:
         '''method to save updated data'''
         self.updated_at = datetime.now()
         '''call save(self) method of storage'''
         models.storage.save()
 
-
     def to_dict(self):
-
         '''method to store obj in dictionary'''
         air_dict = self.__dict__.copy()
         air_dict['__class__'] = self.__class__.__name__
         air_dict['created_at'] = self.created_at.isoformat()
         air_dict['updated_at'] = self.updated_at.isoformat()
         return air_dict
-    
