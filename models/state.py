f4033d011c5f39ab3184bcdcaf8cb325dbba9c4d
root <sonya12slyvia@gmail.com>
2024-05-19 15:50:31 -0700

    pycode

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''This module is the State class'''
-from models.base_model import BaseModel 
+from models.base_model import BaseModel
 
 
 class State (BaseModel):
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''This module is the User class'''
-from models.base_model import BaseModel 
+from models.base_model import BaseModel
+
 
 class User(BaseModel):
     '''User class inherits from BaseModel'''
