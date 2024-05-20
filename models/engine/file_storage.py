b4fc920b41a61662b39205f60924a1e78ffaa2ea
root <sonya12slyvia@gmail.com>
2024-05-19 21:26:42 -0700

    file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,29 +17,49 @@ import os
 
 
 class FileStorage:
+    """Class for serializing and deserializing instances"""
+
     __file_path = os.path.join("./models", "file.json")
     __objects = {}
 
+    classes = {
+        'BaseModel': BaseModel,
+        'Place': Place,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review
+    }
+
     def all(self):
+        """Return the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        """Set obj in __objects with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as f:
-            json.dump(obj_dict, f)
+        """Serialize __objects to the JSON file (path: __file_path)"""
+        obj_serialized = {}
+        for key, value in self.__objects.items():
+            obj_serialized[key] = value.to_dict()
+
+        with open(self.__file_path, "w") as file:
+            json.dump(obj_serialized, file, indent=2)
 
     def reload(self):
+        """Deserialize the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f:
-                obj_dict = json.load(f)
-                for key, value in obj_dict.items():
-                    cls_name = value['__class__']
-                    cls = globals().get(cls_name)
-                    if cls:
-                        self.__objects[key] = cls(**value)
+            with open(self.__file_path, "r") as file:
+                obj_content = json.load(file)
+
+                for key, value in obj_content.items():
+                    class_name = value.get("__class__")
+
+                    if class_name in self.classes:
+                        new_instance = self.classes[class_name](**value)
+                        self.__objects[key] = new_instance
         except FileNotFoundError:
             pass
