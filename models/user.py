598e2ef7856fb9dab7c02a4f6567447f2d1e317c
chisonia <sonya12slyvia@gmail.com>
2024-05-21 09:40:35 +0100

    corrected Username

--- models/user.py
+++ models/user.py
@@ -6,16 +6,18 @@ from models.base_model import BaseModel
 class User(BaseModel):
     '''User class inherits from BaseModel'''
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, email='', first_name='', last_name='', password='', *args, **kwargs):
          super().__init__(*args, **kwargs)
-         self.email = ''
-         self.first_name = ''
-         self.last_name = ''
-         self.paswword = ''
+         self.email = email
+         self.first_name = first_name
+         self.last_name = last_name
+         self.paswword = password
 
     def __str__(self) -> str:
         return (
             'email: {}, first_name: {}, last_name: {}, password: {}'.format(
             self.email, self.first_name, self.last_name, self.paswword
             )
-            )      
+            )
+
+        
